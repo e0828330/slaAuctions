bc6129777cbcb475561d4d6e83ea9e5371dfce2b
Adel Gadllah <adel.gadllah@gmail.com>
2013-12-22 19:39:20 +0100

    Add dutch customer

---
+++ slaAuctions/src/main/java/slaAuctions/agents/DutchCustomer.java
@@ -0,0 +1,25 @@
+package slaAuctions.agents;
+
+import org.springframework.context.ApplicationContext;
+
+import slaAuctions.customerBeans.DutchCustomerBean;
+import slaAuctions.entities.Template;
+import slaAuctions.exceptions.TransactionAbortedException;
+
+public class DutchCustomer extends Agent {
+
+	public DutchCustomer(ApplicationContext context, Template template) {
+		super(context, template);
+	}
+
+	public void run() {
+		DutchCustomerBean bean = (DutchCustomerBean) context.getBean("dutchCustomerBean");
+		Template match = bean.waitForMatch(template);
+		try {
+			bean.writeMatch(match.getUid());
+		} catch (TransactionAbortedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ slaAuctions/src/main/java/slaAuctions/customerBeans/DutchCustomerBean.java
@@ -0,0 +1,62 @@
+package slaAuctions.customerBeans;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.openspaces.core.GigaSpace;
+import org.openspaces.core.context.GigaSpaceContext;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import slaAuctions.entities.Match;
+import slaAuctions.entities.Template;
+import slaAuctions.exceptions.TransactionAbortedException;
+
+import com.j_spaces.core.client.SQLQuery;
+
+@Transactional
+public class DutchCustomerBean {
+	@GigaSpaceContext
+	private GigaSpace space;
+
+	public Template waitForMatch(Template tpl) {
+		String queryString = "";
+		
+		int i = 0;
+		for (i = 0; i < 9; i++) {
+			Integer min;
+			Integer max;
+			try {
+				min = (Integer) PropertyUtils.getSimpleProperty(tpl, "property" + i + "_min");
+				max = (Integer) PropertyUtils.getSimpleProperty(tpl, "property" + i + "_max");
+				if (min != null && max != null) {
+					if (!queryString.isEmpty()) {
+						queryString += " AND ";
+					}
+					queryString += " property" + i + "_current BETWEEN " + min + " AND " + max;
+				}
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		queryString += " AND price <= " + tpl.getPrice_max();
+
+		return space.read(new SQLQuery<Template>(Template.class, queryString), Integer.MAX_VALUE);
+	}
+	
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	public void writeMatch(String tplId) throws TransactionAbortedException {
+		Template tpl = space.takeById(Template.class, tplId);
+		if (tpl == null) {
+			throw new TransactionAbortedException("Template no longer in the space!");
+		}
+		space.write(new Match(tpl.getProviderId()));
+	}
+	
+}
